d00869d67cb1cf7378415673b53fa2d3a7ec3ae4
salonirk11 <salonirk11@gmail.com>
2018-07-16 18:54:09 +0530

    add cmake support

---
+++ scheck/build/CMakeFiles/scheck.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scheck.dir/src/main.cpp.o"
+  "scheck.pdb"
+  "scheck"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scheck.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
